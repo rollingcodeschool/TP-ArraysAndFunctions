2ed52360d982ee1eaf9a70f80df8f7c15dcd6e1c
Emilse <emilsearias@gmail.com>
2021-10-18 14:00:03 -0300

    se agrego el codigo del punto 1 del tp

---
+++ README.md
@@ -0,0 +1,10 @@
+# Practica de javascript - Arreglos y funciones 
+
+*Este repositorio contiene ejercicios basicos donde utilizamos arreglos, bucles, variables y funciones*
+
+### ¿Cómo probar los ejercicios? 📋
+
+Para probar los ejercicios podes reemplazar cada archivo de js en index.html para ver como funciona cada punto.
+___
+
+Made with ❤️ by [Emilse Arias](https://github.com/earias08)
---
+++ js/ejercicio1.js
@@ -0,0 +1,10 @@
+// 1-Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista  los doce nombres del arreglo
+
+let month = ['Enero','Febrero','Marzo', 'Abril', 'Mayo','Junio','Julio','Agosto', 'Septiembre','Octubre','Noviembre','Diciembre' ];
+
+document.write('<h2>Lista de Meses</h2>');
+document.write('<ul>');
+for(let i=0; i < month.length; i++){
+    document.write(`<li>${month[i]}</li>`)
+}
+document.write('</ul>');
